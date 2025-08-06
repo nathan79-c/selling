bbe1afca80ad3a5e2b9e2723de75a15a2f3771df
nathan79-c <19kt210@esisalama.org>
2025-08-06 14:41:03 +0200

    start navigation for screen

--- app/src/main/java/com/example/selling/ui/screens/MaBoutique.kt
+++ app/src/main/java/com/example/selling/ui/screens/MaBoutique.kt
@@ -1,2 +1,19 @@
 package com.example.selling.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun BoutiqueApp(){
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "",
+    ){
+        composable<>{
+    }
+}
