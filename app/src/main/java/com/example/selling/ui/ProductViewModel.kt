bf443b3fc5f77f2dd72d6fd5599100f687b527da
nathan79-c <19kt210@esisalama.org>
2025-08-08 16:46:38 +0200

    creation o product ui state

--- app/src/main/java/com/example/selling/ui/ProductViewModel.kt
+++ app/src/main/java/com/example/selling/ui/ProductViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.selling.ui
 
+
+
 class ProductViewModel {
+}
+
+sealed class ProductUiState{
+    data object Loading: ProductUiState()
+    data object Success: ProductUiState()
+    data class Error(val message: String): ProductUiState()
+
 }
