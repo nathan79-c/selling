e60df170a511bd5cd38f62ba1735709f3b1cb438
nathan79-c <19kt210@esisalama.org>
2025-08-14 09:06:43 +0200

    creation of injection of dependecy whith koin

---
+++ app/src/main/java/com/example/selling/ui/MainApplication.kt
@@ -0,0 +1,23 @@
+package com.example.selling.ui
+
+import android.app.Application
+import com.example.selling.ui.di.myAppModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@MainApplication)
+            // Load modules
+            modules(myAppModules)
+        }
+    }
+}
--- app/src/main/java/com/example/selling/ui/di/AppModule.kt
+++ app/src/main/java/com/example/selling/ui/di/AppModule.kt
@@ -1,4 +1,13 @@
 package com.example.selling.ui.di
 
+import com.example.selling.data.repository.AuthRepository
+import com.example.selling.ui.viewModel.AuthViewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
+val myAppModules = module {
+    viewModelOf(::AuthViewModel)
+    single { AuthRepository(get()) }
+
+}
