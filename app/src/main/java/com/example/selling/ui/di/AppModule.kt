313d26e6942e2ae0883b86bd27996ba2f3ef2683
nathan79-c <19kt210@esisalama.org>
2025-08-14 09:10:23 +0200

    creation of firebase authentification

--- app/src/main/java/com/example/selling/ui/di/AppModule.kt
+++ app/src/main/java/com/example/selling/ui/di/AppModule.kt
@@ -2,12 +2,14 @@ package com.example.selling.ui.di
 
 import com.example.selling.data.repository.AuthRepository
 import com.example.selling.ui.viewModel.AuthViewModel
-import org.koin.core.context.startKoin
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val myAppModules = module {
     viewModelOf(::AuthViewModel)
     single { AuthRepository(get()) }
+    factory { Firebase.auth(get()) } // Firebase Auth
 
 }
