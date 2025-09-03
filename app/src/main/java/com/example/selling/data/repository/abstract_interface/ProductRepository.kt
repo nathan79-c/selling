297a11ef620a65829a7e3ee0ff2ed594c5b84797
nathan79-c <19kt210@esisalama.org>
2025-09-03 13:11:21 +0200

    implementation of creation of product

--- app/src/main/java/com/example/selling/data/repository/abstract_interface/ProductRepository.kt
+++ app/src/main/java/com/example/selling/data/repository/abstract_interface/ProductRepository.kt
@@ -1,4 +1,14 @@
 package com.example.selling.data.repository.abstract_interface
 
+import com.example.selling.data.model.Product
+
 interface ProductRepository {
+
+    suspend fun createProduct(product: Product): Result<Unit>
+    suspend fun AllProductWithoutMy(): Result<List<Product>>
+    suspend fun MyAllProduct(): Result<List<Product>>
+    suspend fun updateProduct(product: Product): Result<Unit>
+    suspend fun deleteProduct(productId: String): Result<Unit>
+
+
 }
